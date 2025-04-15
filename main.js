da8f3993920e6f63164f68ce12e88b4680daff0f
OurPCParts <125259980+OurPCParts@users.noreply.github.com>
2025-04-15 19:16:21 +0200

    main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function addToCart() {
-    var kosarUzenet = document.getElementById('kosar-uzenet');
-    kosarUzenet.style.display = 'block'; // Megjeleníti az üzenetet
-  
-    // Eltünteti az üzenetet 3 másodperc után
-    setTimeout(function() {
-      kosarUzenet.style.display = 'none';
-    }, 3000);
-  }
-  
+
